42e7bdac8071c31bb0feaab4c3cd0edf10f2b420
Madhuri S <madhusabbineni@gmail.com>
2024-01-03 20:30:23 -0600

    update in version of dependencies and few styling changes on landing page.

--- src/components/mainpage/Mainpage.js
+++ src/components/mainpage/Mainpage.js
@@ -7,23 +7,21 @@ const Mainpage = (props) => {
     <main className={classes.main}>
       <div>
         <h2>About App</h2>
-        <br />
         <p>
           Organising your things will become easy. Manage your time more
           effectively and stay on-track towards your goals.
         </p>
-        <br />
         <p>
           Organizing isn't about perfection. It's about efficiency, reducing
           stress and clutter,saving time and money, and improving your overall
           quality of life.
         </p>
-        <br />
+
         <p>
           Making sure to be on track for your plans is important than planning
           itself. Check goals, review and revise them frequently.
         </p>
-        <br />
+
         <p>
           Planning for professional, personal including physical and mental
           growth are needed to be kept in mind when creating goals.
